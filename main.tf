0f9a070702e09a08613c8cf7513430acd07f58d3
masktv <masktvgit@gmail.com>
2025-04-09 16:53:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -335,11 +335,11 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   price_class = "PriceClass_All"
 
-  # viewer_certificate {
-  #   acm_certificate_arn = var.certificate_arn # Use the same certificate as your ALB if needed
-  #   ssl_support_method  = "sni-only"
-  #   minimum_protocol_version = "TLSv1.2_2019"
-  # }
+   viewer_certificate {
+     acm_certificate_arn = var.certificate_arn # Use the same certificate as your ALB if needed
+     ssl_support_method  = "sni-only"
+     minimum_protocol_version = "TLSv1.2_2019"
+   }
 
   restrictions {
     geo_restriction {
