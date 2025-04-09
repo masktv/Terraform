932835902324e4a7346a8df8219caffb524b8c6a
masktv <masktvgit@gmail.com>
2025-04-09 15:10:06 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,82 +1,87 @@
 variable "region" {
     description = "select accoreding to location"
     type = string
-    default = "ap-south-1"
+    default = "ap-southeast-1"
 }
 
 variable "vpc_cidr_block" {
     type = string
-    default "....."
+    default "172.16.0.0/16"
 }
 
 variable "pub_sub_cidr_a" {
     type = string
-    default "....."
+    default "172.16.1.0/24"
 }
 
 variable "pub_sub_cidr_b" {
     type = string
-    default "....."
+    default "172.16.2.0/24"
 }
 
 variable "pub_sub_cidr_c" {
     type = string
-    default "....."
+    default "172.16.3.0/24"
 }
 
 variable "priv_sub_cidr_a" {
     type = string
-    default "....."
+    default "172.16.101.0/24"
 }
 
-variable "priv_sub_cidr_a" {
+variable "priv_sub_cidr_b" {
     type = string
-    default "....."
+    default "172.16.102.0/24"
 }
 
 variable "priv_sub_cidr_c" {
     type = string
-    default "....."
+    default "172.16.103.0/24"
 }
 
 # deployment template image_id
 variable "image_id" {
     type = string
-    default = "...."
+    default = "ami-065a492fef70f84b1"
 }
 
 variable "instance_type" {
     type = string
-    default = "...."
+    default = "t2.micro"
 }
 
-variabe "volume_size" {
+variable "volume_size" {
     type = number
-    default = "....."
+    default = "8"
 }
 
-variable "ssl_certificate_arn" {
-    type = string 
-    default = "..."
-}
+#variable "ssl_certificate_arn" {
+ #   type = string 
+  #  default = "..."
+#}
 
 variable "database_ami" {
     type = string
-    default = "...."
+    default = "ami-065a492fef70f84b1"
 }
 
 variable "instance_type_db" {
     type = string
-    default = "...."
+    default = "t2.micro"
 }
 
 variable "volume_size_db" {
     type = string
-    default = "...."
+    default = "8"
 }
 
 
 variable "aliases" {
-    type = string
-    default = "...."
+    type    = list(string)
+    default = [
+        "web.masktvott.com",
+        "admin.masktvott.com",
+        "spain.masktvott.com",
+        "masktvott.com"
+    ]
 }
