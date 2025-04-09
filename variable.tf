a4a1c777e54b258edf31ba6fcd353de25f1ad248
masktv <masktvgit@gmail.com>
2025-04-09 16:42:07 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,86 +1,85 @@
 variable "region" {
-    description = "select accoreding to location"
-    type = string
-    default = "ap-southeast-1"
+  description = "select accoreding to location"
+  type        = string
+  default     = "ap-southeast-1"
 }
 
 variable "vpc_cidr_block" {
-    type = string
-    default "172.16.0.0/16"
+  type    = string
+  default = "172.16.0.0/16"
 }
 
 variable "pub_sub_cidr_a" {
-    type = string
-    default "172.16.1.0/24"
+  type    = string
+  default = "172.16.1.0/24"
 }
 
 variable "pub_sub_cidr_b" {
-    type = string
-    default "172.16.2.0/24"
+  type    = string
+  default = "172.16.2.0/24"
 }
 
 variable "pub_sub_cidr_c" {
-    type = string
-    default "172.16.3.0/24"
+  type    = string
+  default = "172.16.3.0/24"
 }
 
 variable "priv_sub_cidr_a" {
-    type = string
-    default "172.16.101.0/24"
+  type    = string
+  default = "172.16.101.0/24"
 }
 
 variable "priv_sub_cidr_b" {
-    type = string
-    default "172.16.102.0/24"
+  type    = string
+  default = "172.16.102.0/24"
 }
 
 variable "priv_sub_cidr_c" {
-    type = string
-    default "172.16.103.0/24"
+  type    = string
+  default = "172.16.103.0/24"
 }
 
 # deployment template image_id
 variable "image_id" {
-    type = string
-    default = "ami-065a492fef70f84b1"
+  type    = string
+  default = "ami-065a492fef70f84b1"
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "volume_size" {
-    type = number
-    default = "8"
+  type    = number
+  default = 8
 }
 
-#variable "ssl_certificate_arn" {
- #   type = string 
-  #  default = "..."
-#}
+variable "certificate_arn" {
+  type    = string
+  default = "" # Replace with your actual certificate ARN
+}
 
 variable "database_ami" {
-    type = string
-    default = "ami-065a492fef70f84b1"
+  type    = string
+  default = "ami-065a492fef70f84b1"
 }
 
 variable "instance_type_db" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "volume_size_db" {
-    type = string
-    default = "8"
+  type    = string
+  default = "8"
 }
 
-
 variable "aliases" {
-    type    = list(string)
-    default = [
-        "web.masktvott.com",
-        "admin.masktvott.com",
-        "masktvott.com"
-    ]
+  type    = list(string)
+  default = [
+    "web.masktvott.com",
+    "admin.masktvott.com",
+    "masktvott.com",
+  ]
 }
