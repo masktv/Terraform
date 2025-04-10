d4fe889f7954a98313e8e6481b7656cd6779a4ee
masktv <masktvgit@gmail.com>
2025-04-10 16:26:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -155,14 +155,14 @@ resource "aws_security_group" "custom_sg" {
   vpc_id = aws_vpc.custom_vpc.id
   ingress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # -1 means all protocols
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
