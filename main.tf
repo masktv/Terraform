a951ba4f9935a6f3414982e3cd8a738351565055
masktv <masktvgit@gmail.com>
2025-04-10 17:35:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -231,6 +231,7 @@ resource "aws_lb_target_group" "target_group" {
 }
 
 resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.deployment_loadbalancer.arn
   port     = 80
   protocol = "HTTP"
   default_action {
