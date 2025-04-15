81944d247ee6156bb182d8499121c4bbad2000e6
masktv <masktvgit@gmail.com>
2025-04-15 10:19:15 +0530

    Update app-file.sh

--- app-file.sh
+++ app-file.sh
@@ -4,7 +4,7 @@
 apt-get update -y
 
 # Install EFS mount helper (amazon-efs-utils)
-apt install -y nfs-common
+apt install nfs-common -y
 
 # Add EFS DNS entry to /etc/fstab
 echo "${efs_dns}:/ /var/www/html nfs4 defaults,_netdev 0 0" >> /etc/fstab
