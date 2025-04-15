3226d87c069ce2b5380a7ecf1a68d2a66bf79d10
masktv <masktvgit@gmail.com>
2025-04-15 13:11:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+# manage backend on s3 bucket
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
+
+# mention cloud provider
 provider "aws" {
   region = var.region
 }
