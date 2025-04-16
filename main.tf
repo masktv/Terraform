f8b306b2ef6ba9f77a291e185a8c2a2be8730a15
masktv <masktvgit@gmail.com>
2025-04-16 10:39:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -382,12 +382,12 @@ resource "aws_efs_mount_target" "public_sub_3" {
 }
 
 # DB EFS
-resource "aws_efs_file_system" "db_file_efs" {
-  creation_token = "db-data-efs"
+resource "aws_efs_file_system" "app_log_efs" {
+  creation_token = "app-log-efs"
   encrypted      = true
 
   tags = {
-    Name = "db-file"
+    Name = "app-log-efs"
   }
 
   lifecycle {
@@ -395,22 +395,22 @@ resource "aws_efs_file_system" "db_file_efs" {
   }
 }
 
-# DB EFS Mount Targets
-resource "aws_efs_mount_target" "private_sub_1" {
-  file_system_id  = aws_efs_file_system.db_file_efs.id
-  subnet_id       = aws_subnet.private_subnet_1.id
+# LOG EFS Mount Targets
+resource "aws_efs_mount_target" "public_sub_1" {
+  file_system_id  = aws_efs_file_system.app_log_efs.id
+  subnet_id       = aws_subnet.public_subnet_1.id
   security_groups = [aws_security_group.custom_sg.id]
 }
 
-resource "aws_efs_mount_target" "private_sub_2" {
-  file_system_id  = aws_efs_file_system.db_file_efs.id
-  subnet_id       = aws_subnet.private_subnet_2.id
+resource "aws_efs_mount_target" "public_sub_2" {
+  file_system_id  = aws_efs_file_system.app_log_efs.id
+  subnet_id       = aws_subnet.public_subnet_2.id
   security_groups = [aws_security_group.custom_sg.id]
 }
 
-resource "aws_efs_mount_target" "private_sub_3" {
-  file_system_id  = aws_efs_file_system.db_file_efs.id
-  subnet_id       = aws_subnet.private_subnet_3.id
+resource "aws_efs_mount_target" "public_sub_3" {
+  file_system_id  = aws_efs_file_system.app_log_efs.id
+  subnet_id       = aws_subnet.public_subnet_3.id
   security_groups = [aws_security_group.custom_sg.id]
 }
 
