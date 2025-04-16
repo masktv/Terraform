fb9a27b9423dc51ebe8d57d324efce38826eaeb3
masktv <masktvgit@gmail.com>
2025-04-16 10:59:54 +0530

    Update app-file.sh

--- app-file.sh
+++ app-file.sh
@@ -8,8 +8,11 @@ apt install nfs-common -y
 
 # Add EFS DNS entry to /etc/fstab
 echo "${efs_dns}:/ /var/www/html nfs4 defaults,_netdev 0 0" >> /etc/fstab
+echo "${log_dns}:/ /var/log/apache2 nfs4 defaults,_netdev 0 0" >> /etc/fstab
+
+# reload daemon 
 systemctl daemon-reload
 sleep 60
 # Mount all filesystems
 mount -a
-
+log_dns
